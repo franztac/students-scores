1d7a7bd3c1c6eab6cea261406f69a04420a9851d
franztac <fratacc91@gmail.com>
2025-03-03 18:59:31 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = "-e ."
+
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    this function will return the list of requirements
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+pippo = get_requirements("requirements.txt")
+pippo.remove("seaborn")
+
+setup(
+    name="ml_project",
+    version="0.0.1",
+    author="franz",
+    author_email="fratacc91@gmail.com",
+    packages=find_packages(),
+    # install_requires=["pandas", "numpy", "seaborn"],
+    install_requires=get_requirements("requirements.txt"),
+)
